eac6f74a8aacfa28973b63c233d014b8df776505
Mizeroo <l.mizero@alustudent.com>
2026-04-23 03:53:13 -0700

    remove e and q

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord("a"), ord("z") + 1):
-    if i == 113:
+    if i == ord("q") or  i == ord("e"):
         continue
-    if i == 101:
-        continue
-print("{}".format(chr(i)), end="")
+    print("{}".format(chr(i)), end="")
