7061d06fa2f1d3ee3e9a8a46e7bc4b097be97f20
Mizeroo <l.mizero@alustudent.com>
2026-04-23 04:25:43 -0700

    print number in doubel form

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 99):
     if i == 98:
-        print("{}".format(i:,2%0),)
-    print("{}".format(i:,2%0), end="")
+        print("{}".format(i: , 2%0))
+    print("{}".format(i: , 2%0), end="")
