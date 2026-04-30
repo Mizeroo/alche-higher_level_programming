21ef076b5abf5bcd8af2434c9eb0abb832e0ca2a
Mizeroo <l.mizero@alustudent.com>
2026-04-30 05:00:16 -0700

    search and replce

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,6 +3,6 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
         if i == search:
-        new_list.append(replace)
+            new_list.append(replace)
     else:
         return(new_list(i)
