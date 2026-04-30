1187db96656462f31a832b6ca70d6d4562cc524f
Mizeroo <l.mizero@alustudent.com>
2026-04-30 11:55:07 -0700

    multiplicatio

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
     for i in a_dictionary:
-        new_dictionary.append(map(lambda x: x*2, a_dictionary))
-    return(new_dictionary)
+        new_dictionary[i] = a_dictionary[i] * 2
+    return new_dictionary
