31febfb311632e8f555edd6e819bccfdec534d2b
Mizeroo <l.mizero@alustudent.com>
2026-05-05 06:19:16 -0700

    raising error withn message

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     try:
-        raise NameError("C is fun")
-    except NameError as ne:
-        print("Python is cool")
+        raise NameError(message)
