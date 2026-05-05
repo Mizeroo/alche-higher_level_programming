b140b8d9215f6c21182c2fa8f697e30dfbaf2035
Mizeroo <l.mizero@alustudent.com>
2026-05-05 05:07:11 -0700

    Division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    quotient = none
+    quotient = None
     try:
         quotient = a / b
         print("Inside result: {}".format( quotient))
