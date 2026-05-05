75badb069965d13ffde65ce328740eac6154c420
Mizeroo <l.mizero@alustudent.com>
2026-05-05 04:23:08 -0700

    same example as 0

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
         for i in range(x):
-            print(my_list[i], end="")
+            print("{:d}".format(my_list[i]), end="")
     except (ValueError, IndexError):
         pass
     finally:
