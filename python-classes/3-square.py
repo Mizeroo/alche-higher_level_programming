56af522899232afd472a0293d5754c340441da0f
Mizeroo <l.mizero@alustudent.com>
2026-05-13 13:57:20 +0400

    Remove unnecessary blank lines in Square class

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Module that defines a Square class."""
- 
- 
+
+
 class Square:
     """Defines a square by its size."""
- 
+
     def __init__(self, size=0):
         """Instantiate a Square with an optional size.
- 
+
         Args:
             size (int): The size of the square. Defaults to 0.
- 
+
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
@@ -20,7 +20,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
- 
+
     def area(self):
         """Return the current area of the square."""
         return self.__size ** 2
