aeb3caf29b32f3abb1d75c16907b5266d65071f0
Mizeroo <l.mizero@alustudent.com>
2026-05-13 13:52:07 +0400

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 """Module that defines a Square class."""
- 
- 
 class Square:
     """Defines a square by its size."""
- 
     def __init__(self, size):
         """Instantiate a Square with a size.
- 
         Args:
             size (int): The size of the square.
         """
