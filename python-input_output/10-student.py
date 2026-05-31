3a2c17596cb12bb450ceeae7e1a3b0ffdb37e524
Mizeroo <l.mizero@alustudent.com>
2026-05-31 09:41:14 +0400

    Add files via upload

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module defining the Student class with filtered JSON support."""
+
+
+class Student:
+    """A class that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance with first_name, last_name and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only those attributes are returned.
+        Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
