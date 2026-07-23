40b4defa3d633df0f0ef2691c25bd5c6177745fa
unknown <you@example.com>
2026-07-23 15:36:59 +0400

    loops

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let languages = ['C is fun", "Python is cool", "JavaScript is amazing'];
+let output = '';
+
+for (let i = 0; i < languages.length; i++) {
+  output = output + languages[i] + "\n";
+}
+console.log(output);
+
