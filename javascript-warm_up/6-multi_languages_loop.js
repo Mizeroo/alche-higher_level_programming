32baebaa1c4f9ac5e8a13e8488a15395cc815b50
Mizeroo <l.mizero@alustudent.com>
2026-07-23 15:40:11 +0400

    Update 6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-let languages = ['C is fun", "Python is cool", "JavaScript is amazing'];
+const languages = ['C is fun", 'Python is cool', "JavaScript is amazing'];
 let output = '';
-
 for (let i = 0; i < languages.length; i++) {
   output = output + languages[i] + "\n";
 }
 console.log(output);
-
