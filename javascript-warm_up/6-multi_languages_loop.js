59bda275930d55a76da17603b690b562de6c7913
Mizeroo <l.mizero@alustudent.com>
2026-07-23 15:41:55 +0400

    Fix loop to correctly append language strings

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,6 @@
 const languages = ['C is fun", 'Python is cool', "JavaScript is amazing'];
 let output = '';
 for (let i = 0; i < languages.length; i++) {
-  output = output + languages[i] + "\n";
-}
+  output = output + languages[i] + '\n'
+};
 console.log(output);
