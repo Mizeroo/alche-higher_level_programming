63d9518aa69c2ad7bdfab72def8cd47efc1cb6e4
Mizeroo <l.mizero@alustudent.com>
2026-07-26 04:51:31 +0400

    Update 6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@
 const SquareBase = require('./5-square');
 
 class Square extends SquareBase {
-  charPrint(c) {
+  charPrint (c) {
     const char = c === undefined ? 'X' : c;
     for (let i = 0; i < this.height; i++) {
       console.log(char.repeat(this.width));
