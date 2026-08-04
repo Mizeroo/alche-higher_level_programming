18d6c0672d564bff1bf195fa8b503e72c5dee457
Mizeroo <l.mizero@alustudent.com>
2026-08-05 03:15:56 +0400

    Update 2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
   console.log(`code: ${response.statusCode}`);
 });
